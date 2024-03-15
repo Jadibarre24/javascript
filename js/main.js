2c4098477d129f4852c074cfd1799aae729b99b1
jadibarre24 <andy024.jd@gmail.com>
2024-03-14 22:48:41 -0500

    funcion-inicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-let = productos 
+alert ("Bienvenido")
+const rango = ( Number >=18 )
+function datosPacientes(){
+let nombre  = (prompt("Cual es tu nombre"))
+let edad  = Number (prompt("Que edad tienes " ))
+}
+datosPacientes()
+ 
+if(nombre == " ")
+alert("Ingrese nombre")
+else if(edad == " " )
+alert("Ingrese edad")
+else (
+alert ("En que te puedo ayudar " + nombre ))
+
+
+confirm ("Tiene dolor")
+let sintomas = true
+if (sintomas=== true)
+alert("En que parte del cuerpo")
+else if(sintomas == false )
+alert("Error de confirmacion ")
+else(
+    alert("Error de confirmacion ")
+    )
+    let ubicacion = [cabeza , estomago , musculos , otro ] 
+    
+
+console.table(tipeof, sintomas)
+
+
+
