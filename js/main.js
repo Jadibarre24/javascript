263fa964cd44dd1ec50e95fdb8827461c3c4ce06
jadibarre24 <andy024.jd@gmail.com>
2024-03-18 20:45:56 -0500

    triage-inicial

--- js/main.js
+++ js/main.js
@@ -1,32 +1,45 @@
-alert ("Bienvenido")
-const rango = ( Number >=18 )
 function datosPacientes(){
-let nombre  = (prompt("Cual es tu nombre"))
-let edad  = Number (prompt("Que edad tienes " ))
+    let nombre  = (prompt("Cual es tu nombre"))
+    let edad  = Number (prompt("Que edad tienes " ))
+    if((nombre == "") || (edad == "" ))
+    alert("Datos incorrectos.")
+    else if (edad <= 18)
+    alert("ingrese edad.")
+    else
+    alert ("bienvenido " + nombre)
 }
+function detalles() {
+    const ubicacion =  ["Cabeza" , "Estomago" , "Musculos" ];
+    confirm (ubicacion [0])
+    if (ubicacion [0]===true)
+    alert("Tienes atencion de prioridad media. espere su llamado")
+    else if (ubicacion [0]=== false)
+    confirm (ubicacion [1])
+    else if (ubicacion [1]===true)
+    alert("Tienes atencion de prioridad alta. espere su llamado")
+    else if (ubicacion [1]===false)
+    confirm (ubicacion [2])
+    else if (ubicacion [2]===true)
+    alert("Tienes atencion de prioridad baja. espere su llamado")
+    else if (ubicacion [2]===false)
+    alert ("Espere en la sala.")
+else(
+    alert("El medico en un momento lo atendera. ")
+    )    
+} 
+
+alert ("Bienvenido")
 datosPacientes()
- 
-if(nombre == " ")
-alert("Ingrese nombre")
-else if(edad == " " )
-alert("Ingrese edad")
-else (
-alert ("En que te puedo ayudar " + nombre ))
-
-
-confirm ("Tiene dolor")
-let sintomas = true
+let sintomas = confirm ("Tiene dolor")
 if (sintomas=== true)
 alert("En que parte del cuerpo")
-else if(sintomas == false )
+else if(sintomas === false )
 alert("Error de confirmacion ")
 else(
-    alert("Error de confirmacion ")
+    alert("En que parte del cuerpo le duele ")
     )
-    let ubicacion = [cabeza , estomago , musculos , otro ] 
-    
-
-console.table(tipeof, sintomas)
+detalles()
+ 
 
 
 
